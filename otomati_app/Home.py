4b94e4cca00ebbe2d6c661ad5096d395385cad67
Justin Hung <hugh.12345zxcvb@gmail.com>
2024-06-21 17:11:07 +0800

    ci: init githubci and test

--- otomati_app/Home.py
+++ otomati_app/Home.py
@@ -15,7 +15,7 @@ st.title(title)
 
 
 # Render sidebar and get the OpenAI API key
-openai_api_key = os.getenv("OPENAI_API_KEY") or render_sidebar()
+openai_api_key = render_sidebar()
 
 
 def generate_response(t):
