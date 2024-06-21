30576d16b9fc57ead9daaf0c1566533875a9c095
Justin Hung <hugh.12345zxcvb@gmail.com>
2024-06-21 17:44:44 +0800

    fix: imported but unused lib and allow empty test report

--- otomati_app/Home.py
+++ otomati_app/Home.py
@@ -1,5 +1,3 @@
-import os
-
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
@@ -12,7 +10,6 @@ load_dotenv()
 
 title = "Otomati APP"
 st.title(title)
-
 
 # Render sidebar and get the OpenAI API key
 openai_api_key = render_sidebar()
